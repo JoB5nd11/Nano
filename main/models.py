4def3767ee42179424b160cdd25adc2189ef8e0c
JoB5nd11 <johannesbendler115@gmail.com>
2022-11-06 18:06:09 +0100

    additional file edits

--- main/models.py
+++ main/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+import os
 
 class VideoContent(models.Model):
     media = models.FileField(upload_to='')
+    datetime = models.DateTimeField(default=timezone.now())
--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import VideoContent
 
 def home(request):
-    return render(request, 'main/home.html')
+    ctx = {
+        'videos': VideoContent.objects.all().order_by('-datetime'),
+    }
+    return render(request, 'main/home.html', ctx)
 
 def upload(request):
     if request.method == 'POST':
         vc = VideoContent()
         vc.media = request.FILES["media"]
         vc.save()
+        return HttpResponseRedirect(reverse('nano-home'))
     return render(request, 'main/upload.html')
